2da779f574bc37142754e02b1fb79e7d5e96e84d
Ganesha Danu <ganeshadanu1@gmail.com>
2019-03-09 22:52:28 +0700

    implement csv reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate csv;
+
+use std::error::Error;
+use std::fs;
+
+fn readCSV(from: &str) -> Result<Vec<Vec<String>>, Box<Error>> {
+    let content = fs::read_to_string(from).unwrap();
+    let mut reader = csv::Reader::from_reader(content.as_bytes());
+    let mut data: Vec<Vec<String>> = Vec::new();
+    for result in reader.records() {
+        let result = result?;
+        let mut newRow:Vec<String> = Vec::new();
+        for col in &result { 
+           newRow.push(String::from(col));
+        }
+        data.push(newRow);
+    }
+    Ok(data)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let trainData = readCSV("src/data/TrainData.csv").unwrap();
+    let testData = readCSV("src/data/TestData.csv").unwrap();
+   println!("{:?}", trainData[0]);
+   println!("{:?}", testData[0]);
 }
