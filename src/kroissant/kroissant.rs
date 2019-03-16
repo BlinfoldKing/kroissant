1f5ae77e1ac2b3671ca8c70efc475153c0f08b50
Ganesha Danu <ganeshadanu1@gmail.com>
2019-03-16 23:30:37 +0700

    add value parsing

---
+++ src/kroissant/kroissant.rs
@@ -0,0 +1,21 @@
+pub struct Classifier {
+    data: Vec<Vec<f64>>,
+    class: Vec<f64>
+}
+
+impl Classifier {
+    fn new(data: Vec<Vec<f64>>, class: Vec<f64>) -> Classifier {
+        Classifier {
+            data: data,
+            class: class
+        }
+    }
+
+    fn calculate_distance() {
+    
+    }
+
+    fn get_class() {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,47 @@
 extern crate csv;
 
+mod kroissant;
+
 use std::error::Error;
 use std::fs;
 
-fn readCSV(from: &str) -> Result<Vec<Vec<String>>, Box<Error>> {
+fn read_csv(from: &str) -> Result<Vec<Vec<String>>, Box<Error>> {
     let content = fs::read_to_string(from).unwrap();
     let mut reader = csv::Reader::from_reader(content.as_bytes());
     let mut data: Vec<Vec<String>> = Vec::new();
     for result in reader.records() {
         let result = result?;
-        let mut newRow:Vec<String> = Vec::new();
-        for col in &result { 
-           newRow.push(String::from(col));
+        let mut new_row:Vec<String> = Vec::new();
+        for col in &result {
+           new_row.push(String::from(col));
         }
-        data.push(newRow);
+        data.push(new_row);
     }
     Ok(data)
 }
 
 fn main() {
-    let trainData = readCSV("src/data/TrainData.csv").unwrap();
-    let testData = readCSV("src/data/TestData.csv").unwrap();
-   println!("{:?}", trainData[0]);
-   println!("{:?}", testData[0]);
+    let raw_train_data = read_csv("src/data/TrainData.csv").unwrap();
+    let raw_test_data = read_csv("src/data/TestData.csv").unwrap();
+
+    // convert data into floating point
+    let train_data: Vec<Vec<f64>> =
+        raw_train_data
+        .into_iter()
+        .map(|container| container
+             .into_iter()
+             .map(|value| value.parse().unwrap())
+             .collect())
+        .collect();
+    let test_data: Vec<Vec<f64>> =
+        raw_test_data
+        .into_iter()
+        .map(|container| container
+             .into_iter()
+             .map(|value| value.parse().unwrap())
+             .collect())
+        .collect();
+
 }
+
+
